80f33b997e79b7a8a30a6fe1ab33af040bbdbf77
yoannspace <yoann.moranville@gmail.com>
2019-10-16 17:40:36 +0200

    Modify the homepage for Eliza

--- class/DARIAHFooter.php
+++ class/DARIAHFooter.php
@@ -11,15 +11,15 @@
                         <img src="/wp-content/themes/thefox-child/images/DARIAH-EU-Logo-W.png"/>
                     </div>
                     <div class="about-text">
-                        <div><span>Follow us on:</span> 
+                        <div><span>Follow us on:</span>&nbsp; 
                         	<a href="https://twitter.com/DARIAHeu">
-                        		<img src="https://www-dev.dariah.eu/wp-content/uploads/2019/01/twitter-300x300.png" alt="twitter" width="50" height="50">
-                        	</a>
+                        		<img src="/wp-content/themes/thefox-child/images/twitter-80x80.png" alt="twitter" width="50" height="50">
+                        	</a>&nbsp;
                         	<a href="https://www.youtube.com/channel/UCeQpM_gUvNZXUWf6qQ226GQ?view_as=subscriber">
-                        		<img src="https://www-dev.dariah.eu/wp-content/uploads/2019/01/youtube-logo-300x295.png" alt="youtube" width="50" height="50">
-                        	</a>
+                        		<img src="/wp-content/themes/thefox-child/images/youtube-80x80.png" alt="youtube" width="50" height="50">
+                        	</a>&nbsp;
                         	<a href="https://www.flickr.com/photos/142235661@N08/albums/with/72157695786965901">
-                        		<img src="https://www-dev.dariah.eu/wp-content/uploads/2019/01/flickr-300x269.png" alt="flickr" width="50" height="50">
+                        		<img src="/wp-content/themes/thefox-child/images/flickr-80x80.png" alt="flickr" width="50" height="50">
                         	</a>
                         </div>
                     </div>
